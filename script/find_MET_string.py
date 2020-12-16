7a0b6f02886c7e81b00c0b8fe94db835f56cf816
isa037 <isa37@localhost.localdomain>
2020-12-16 17:38:30 +0100

    Commit the max frequency result and script folder

---
+++ script/find_MET_string.py
@@ -0,0 +1,73 @@
+import sys
+import shutil
+import subprocess
+
+def runSimulation():
+	bashCommand = "/home/isa37/git/lab3/script/start_synopsys"
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+
+def check_MET():
+	datafile = "/home/isa37/git/lab3/syn/analysis_results/timing_results.txt"
+	slackMet = False
+	clockUncertainty = 0.0
+	setupTime = 0.0
+	arrivalTime = 0.0
+	for line in open(datafile):
+		if "data arrival time" in line:
+			lineComponents = line.split()
+			arrivalTime = lineComponents[3]
+			print "data arrival time: " + str(arrivalTime)
+		if "clock uncertainty" in line:
+			lineComponents= line.split()
+			clockUncertainty = lineComponents[2]
+			print "clock uncertainty: " + str(clockUncertainty)
+
+		if "library setup time"	in line:
+			lineComponents= line.split()
+			setupTime = lineComponents[3]
+			print "library setup time: " + str(setupTime)
+			
+		
+	       	if "slack" in line:
+			if "MET" in line:
+	               		slackMet = True
+				break
+	if slackMet:
+		print "Slack is MET"
+		return 0
+	else:
+		print "Slack is VIOLATED"
+		return float(setupTime)+float(clockUncertainty)+float(arrivalTime)
+
+	 
+
+def update_clockValue(new_clock):
+	out = open("/home/isa37/git/lab3/script/new_syntesis_with_clock.tcl", 'w')
+	cnt=0;
+
+	for line in open("/home/isa37/git/lab3/script/syntesis_with_clock.tcl"):
+		if 'create_clock' in line and cnt==0:
+			nuovo = 'create_clock -name MY_CLK -period '+ str(new_clock) +' clk\n'
+			out.write(nuovo)
+			cnt=1;
+		else:
+			out.write(line)
+
+	out.close()
+	shutil.move("/home/isa37/git/lab3/script/new_syntesis_with_clock.tcl","/home/isa37/git/lab3/script/syntesis_with_clock.tcl")
+
+i=0
+newClock = 1111
+while (newClock!=0 and i<15):
+	if newClock==1111 :
+		print "Running first simulation... "
+	else:
+		print "Running simulation with clock: " + str(-newClock)
+	runSimulation()
+
+	newClock = check_MET()
+	print "New clock value is: " + str(newClock)
+	update_clockValue(-newClock)
+	i+=1
+	print "\n------------------------------------------------\n\n"
---
+++ script/syntesis_with_clock.tcl
@@ -0,0 +1,63 @@
+analyze -f vhdl -lib WORK ../src/riscv_pkg.vhd
+analyze -f vhdl -lib WORK ../src/REGN.vhd
+analyze -f vhdl -lib WORK ../src/REGN_PRES.vhd
+analyze -f vhdl -lib WORK ../src/ALU.vhd
+analyze -f vhdl -lib WORK ../src/ALU_CONTROL.vhd
+analyze -f vhdl -lib WORK ../src/BRANCH_VALUE_PROVIDER.vhd
+analyze -f vhdl -lib WORK ../src/COMPARATOR_ID.vhd
+analyze -f vhdl -lib WORK ../src/CONTROL.vhd
+analyze -f vhdl -lib WORK ../src/DATA_MEMORY.vhd
+analyze -f vhdl -lib WORK ../src/FORWARDING_UNIT.vhd
+analyze -f vhdl -lib WORK ../src/HAZARD_UNIT.vhd
+analyze -f vhdl -lib WORK ../src/IMM_GEN.vhd
+analyze -f vhdl -lib WORK ../src/INSTRUCTION_MEMORY.vhd
+analyze -f vhdl -lib WORK ../src/PIPE1_REG.vhd
+analyze -f vhdl -lib WORK ../src/PIPE2_REG.vhd
+analyze -f vhdl -lib WORK ../src/PIPE3_REG.vhd
+analyze -f vhdl -lib WORK ../src/PIPE4_REG.vhd
+analyze -f vhdl -lib WORK ../src/PROGRAM_COUNTER_MANAGER.vhd
+analyze -f vhdl -lib WORK ../src/REGISTER_FILE.vhd
+analyze -f vhdl -lib WORK ../src/WRITE_BACK_SELECTOR.vhd
+analyze -f vhdl -lib WORK ../src/RISCV_PROCESSOR.vhd
+
+
+#Before completing the reading of source we set one parameter to preserve rtl names in the netlist to ease the procedure for power consumption estimation.
+set power_preserve_rtl_hier_names true
+#Launch elaborate command to load the components
+#elaborate <top entity name> -arch <architecture name> -lib WORK > ./elaborate.txt
+elaborate  RISCV_PROCESSOR -arch rtl -lib WORK > ./elaborate_results.txt
+#uniquify #optional command to addres to only 1 specific architecture
+link
+
+#*******  Applying constraints   ***************
+#create 100 Mhz clock
+create_clock -name MY_CLK -period 0 clk
+set_dont_touch_network MY_CLK
+
+#jitter simulation
+set_clock_uncertainty 0.07 [get_clocks MY_CLK]
+
+#input/output delay
+set_input_delay 0.5 -max -clock MY_CLK [remove_from_collection [all_inputs] clk]
+set_output_delay 0.5 -max -clock MY_CLK [all_outputs]
+
+#set output load (buffer x4 used)
+set OLOAD [load_of NangateOpenCellLibrary/BUF_X4/A]
+set_load $OLOAD [all_outputs]
+
+#flatten the hiercarchy
+ungroup -all -flatten
+#implement the pparch multiplier
+#set_implementation DW02_mult/pparch [find cell *mult*]
+
+#*********    Start the syntesis    *************
+compile >  ./analysis_results/compilation_results.txt
+
+#compile ultra
+#compile_ultra >  ./analysis_results/ultra_compilation_results.txt
+
+#*********    Save the results      *************
+report_timing  >  ./analysis_results/timing_results.txt
+report_area    >  ./analysis_results/area_results.txt
+report_resources > ./analysis_results/resource_report.txt
+exit
